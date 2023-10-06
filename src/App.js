970055ae3ebf2e4231d2773498218d9a10ab1441
Sarang <sarangsami75@gmail.com>
2023-10-06 10:18:49 +0330

    feat: :art: Added Bg

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import Box from "./components/Box";
 
 import "./App.css";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 import { Suspense } from "react";
 import AnimatedSphere from "./components/AnimatedSphere";
 import Cloud from "./components/Cloud";
@@ -10,41 +10,41 @@ import Thinkpad from "./components/Thinkpad";
 
 const App = () => {
   return (
-    <>
-     <Canvas className="canvas">
-      <OrbitControls enableZoom={false} />
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[-2, 5, 2]} intensity={1} />
-      <Suspense fallback={null}>
-        <Box />
-      </Suspense>
-    </Canvas>
-    <Canvas className="canvas">
-    <OrbitControls enableZoom={false} />
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[-2, 5, 2]} intensity={1} />
-      <Suspense fallback={null}>
-      <AnimatedSphere/>
-      </Suspense>
-    </Canvas>
-    {/* <Canvas className="canvas">
-    <OrbitControls enableZoom={false} />
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[-2, 5, 2]} intensity={1} />
-      <Suspense fallback={null}>
-      <Cloud/>
-      </Suspense>
-    </Canvas>
-    <Canvas className="canvas">
-    <OrbitControls enableZoom={false} />
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[-2, 5, 2]} intensity={1} />
-      <Suspense fallback={null}>
-      <Thinkpad/>
-      </Suspense>
-    </Canvas> */}
-    </>
-   
+    <div style={{background:"black"}}>
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
+      </Canvas>
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <AnimatedSphere />
+        </Suspense>
+      </Canvas>
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <Stars />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Cloud />
+        </Suspense>
+      </Canvas>
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Thinkpad />
+        </Suspense>
+      </Canvas>
+    </div>
   );
 };
 
